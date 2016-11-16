1b157e28e8fc640be3faa571940da0b51836748f
pranalirasal <pranalirasal@gmail.com>
2016-11-16 11:31:57 +0530

    made some logical changes

--- somebug.c
+++ somebug.c
@@ -1,8 +1,39 @@
-#include<stdio.h>
+// C++ program to print all primes smaller than or equal to
+// n using Sieve of Eratosthenes
+#include <bits/stdc++.h>
+using namespace std;
 
-int main(){
-    int a = 0, b =5, c;
-    c = a/b;
-    
-    printf ("%d", c);
+void SieveOfEratosthenes(int n)
+{
+	// Create a boolean array "prime[0..n]" and initialize
+	// all entries it as true. A value in prime[i] will
+	// finally be false if i is Not a prime, else true.
+	bool prime[n+1];
+	memset(prime, true, sizeof(prime));
+
+	for (int p=2; p*p<=n; p++)
+	{
+		// If prime[p] is not changed, then it is a prime
+		if (prime[p] == true)
+		{
+			// Update all multiples of p
+			for (int i=p*2; i<=n; i += p)
+				prime[i] = true;
+		}
+	}
+
+	// Print all prime numbers
+	for (int p=2; p<=n; p++)
+	if (prime[p])
+		cout << p << " ";
+}
+
+// Driver Program to test above function
+int main()
+{
+	int n = 30;
+	cout << "Following are the prime numbers smaller "
+		<< " than or equal to " << n << endl;
+	SieveOfEratosthenes(n);
+	return 0;
 }
